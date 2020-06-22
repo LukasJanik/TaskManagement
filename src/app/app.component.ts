fece64f1cd6f1a4b3bcd24a13f71446003d8cc82
lukyjanik <luky.janik@gmail.com>
2020-06-22 17:44:56 +0200

    initial version with integrated google log in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SocialAuthService} from "angularx-social-login";
+import {User} from "./store/Entities/User/user.model";
+import {Observable} from "rxjs";
+import {currentUser, State} from "./store";
+import {select, Store} from "@ngrx/store";
+import {map} from "rxjs/operators";
+import {Router} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
+import {userIdCookies} from "./definitions";
+import {getCurrentUser} from "./store/Entities/User/user.actions";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  user$: Observable<User>;
+
+  constructor(
+    private authService: SocialAuthService,
+    private cookieService: CookieService,
+    private router: Router,
+    private store: Store<State>) {
+    this.user$ = this.store.pipe(select(currentUser),
+      map(user => {
+        const googleId = this.cookieService.get(userIdCookies);
+        if (user === null && googleId === null) {
+          router.navigate(['login']);
+        } else if (!!googleId) {
+          this.store.dispatch(getCurrentUser({user: {googleId}}));
+        }
+        return user;
+      }));
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+    this.cookieService.delete(userIdCookies);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,65 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { reducers, metaReducers } from './store';
+import { LoginComponent } from './components/login/login.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {
+  SocialLoginModule,
+  GoogleLoginProvider,
+  SocialAuthServiceConfig
+} from "angularx-social-login";
+import {CookieService} from "ngx-cookie-service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      }
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    SocialLoginModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '329968499656-ctmlqjdad69qkqkko2bqhqlifo7fcibl.apps.googleusercontent.com'
+            ),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
+    CookieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
